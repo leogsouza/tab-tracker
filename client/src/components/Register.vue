c38a88a6f7218e4df6451288cc0aa25b954a7782
Leonardo Souza <leogsouza@gmail.com>
2019-08-15 20:42:22 -0300

    Implement register route and page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" placeholder="Email" v-model="email" />
-    <br />
-    <input type="password" name="password" placeholder="Password" v-model="password" />
-    <br>
-    <button @click="register">Register </button>
-  </div>
+  <v-container>
+    <v-layout column>
+      <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-toolbar flat dense class="cyan" xs6 dark>
+            <v-toolbar-title>Register</v-toolbar-title>
+          </v-toolbar>
+        </div>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <h1>Register</h1>
+          <input type="email" name="email" placeholder="Email" v-model="email" />
+          <br />
+          <input type="password" name="password" placeholder="Password" v-model="password" />
+          <br />
+          <div class="error" v-html="error" />
+          <br />
+          <v-btn @click="register">Register</v-btn>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -15,15 +29,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -31,5 +50,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+ .error {
+   color: red;
+ }
 </style>
